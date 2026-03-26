3a81843c82b1f9c8b11acf6d2d010ec6da16eba9
ale-biaggio <alessandrobiaggioli@gmail.com>
2026-03-26 10:14:37 +0100

    Initial Commit

---
+++ NotifEar Watch App/NotifEarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifEarApp.swift
+//  NotifEar Watch App
+//
+//  Created by Alessandro Biaggioli on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct NotifEar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
